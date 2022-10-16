3f69a43ce839051d2a901f87b24d6c1b83953c73
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:45:29 +0200

    Add files via upload

---
+++ Inicijalno-Sedmi/Inicijalno-Sedmi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Sedmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prim = false;
+            int primBr = 0;
+
+            for (int i=1; i<=100; i++)
+            {
+
+                for (int j=2; j<=100; j++)
+                {
+                    if (i!=j && i%j==0)
+                    {
+                        prim = false;
+                        break;
+                    }
+                }
+                if (prim)
+                {
+                    primBr++;
+                }
+                prim = true;
+
+            }
+            Console.WriteLine(primBr);
+            Console.ReadKey();
+        }
+    }
+}
